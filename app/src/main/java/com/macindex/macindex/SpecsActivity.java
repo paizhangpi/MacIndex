049b7512aafb59f00210e4537c67e3d0e1a678e9
Peizhe Liu <paizhangpi@outlook.con>
2019-12-01 16:21:38 -0600

    initial commit

---
+++ app/src/main/java/com/macindex/macindex/SpecsActivity.java
@@ -0,0 +1,13 @@
+package com.macindex.macindex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpecsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
