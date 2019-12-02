21a33c38f3aa4ae77fd732ccc7266cd75975c38f
paizhangpi <paizhangpi@outlook.com>
2019-12-01 22:56:11 -0600

    Now loading machine names, to fix bug

--- app/src/main/java/com/macindex/macindex/MainActivity.java
+++ app/src/main/java/com/macindex/macindex/MainActivity.java
@@ -2,8 +2,13 @@ package com.macindex.macindex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -11,17 +16,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * MacIndexer Android application and Specs database
+ * University of Illinois, CS125 FA19 Final Project
+ *
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
-    private SQLiteDatabase database;
 
+    private SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initDatabase();
-        database.query("category0", null, null, null, null, null, null, null);
+        initInterface();
     }
 
     private void initDatabase() {
@@ -46,5 +57,20 @@ public class MainActivity extends AppCompatActivity {
         }
         DatabaseOpenHelper dbHelper = new DatabaseOpenHelper(this);
         database = dbHelper.getReadableDatabase();
+    }
+
+    private void initInterface() {
+        View mainChunk = getLayoutInflater().inflate(R.layout.chunk_main, null);
+        TextView machineName = mainChunk.findViewById(R.id.machineName);
+        Button viewButton = mainChunk.findViewById(R.id.viewButton);
+        for (int i = 0; i < 1; i++) {
+            LinearLayout currentLayout = findViewById(R.id.category0Layout);
+            Cursor cursor = database.query("category" + String.valueOf(i), new String[]{"name"},
+                    null, null, null, null, null);
+            while (cursor.moveToNext()) {
+                machineName.setText(cursor.getString(cursor.getColumnIndex("name")));
+                currentLayout.addView(mainChunk);
+            }
+        }
     }
 }
