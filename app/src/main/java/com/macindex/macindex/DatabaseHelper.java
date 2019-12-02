12050b562bee0fe171d5a52a1f2e99c83d548d87
paizhangpi <paizhangpi@outlook.com>
2019-12-01 18:52:18 -0600

    Database helper update

--- app/src/main/java/com/macindex/macindex/DatabaseHelper.java
+++ app/src/main/java/com/macindex/macindex/DatabaseHelper.java
@@ -1,4 +1,119 @@
 package com.macindex.macindex;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * DatabaseHelper Class
+ *
+ * https://stackoverflow.com/questions/513084/ship-an-application-with-a-database
+ * Source acknowledgement as linked above.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private final Context context;
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "specs.db";
+
+    private boolean createDb = false, upgradeDb = false;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    /**
+     * Copy packaged database from assets folder to the database created in the
+     * application package context.
+     *
+     * @param db The target database in the application package context.
+     */
+    private void copyDatabaseFromAssets(SQLiteDatabase db) {
+        Log.i(TAG, "copyDatabase");
+        InputStream myInput = null;
+        OutputStream myOutput = null;
+        try {
+            // Open db packaged as asset as the input stream
+            myInput = context.getAssets().open("path/to/shipped/db/file");
+
+            // Open the db in the application package context:
+            myOutput = new FileOutputStream(db.getPath());
+
+            // Transfer db file contents:
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = myInput.read(buffer)) > 0) {
+                myOutput.write(buffer, 0, length);
+            }
+            myOutput.flush();
+
+            // Set the version of the copied database to the current
+            // version:
+            SQLiteDatabase copiedDb = context.openOrCreateDatabase(
+                    DATABASE_NAME, 0, null);
+            copiedDb.execSQL("PRAGMA user_version = " + DATABASE_VERSION);
+            copiedDb.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new Error(TAG + " Error copying database");
+        } finally {
+            // Close the streams
+            try {
+                if (myOutput != null) {
+                    myOutput.close();
+                }
+                if (myInput != null) {
+                    myInput.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new Error(TAG + " Error closing streams");
+            }
+        }
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i(TAG, "onCreate db");
+        createDb = true;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.i(TAG, "onUpgrade db");
+        upgradeDb = true;
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        Log.i(TAG, "onOpen db");
+        if (createDb) {// The db in the application package
+            // context is being created.
+            // So copy the contents from the db
+            // file packaged in the assets
+            // folder:
+            createDb = false;
+            copyDatabaseFromAssets(db);
+
+        }
+        if (upgradeDb) {// The db in the application package
+            // context is being upgraded from a lower to a higher version.
+            upgradeDb = false;
+            // Your db upgrade logic here:
+        }
+    }
 }
