8ff16c889d442892816804eccf2907400b9d4d60
paizhangpi <paizhangpi@outlook.com>
2019-12-02 17:47:48 -0600

    Experience update

--- app/src/main/java/com/macindex/macindex/MainActivity.java
+++ app/src/main/java/com/macindex/macindex/MainActivity.java
@@ -2,6 +2,8 @@ package com.macindex.macindex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -13,7 +15,6 @@ import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -53,8 +54,13 @@ public class MainActivity extends AppCompatActivity {
             outputStream.flush();
             outputStream.close();
             inputStream.close();
-        } catch (IOException e){
-            // To catch
+        } catch (Exception e) {
+            new AlertDialog.Builder(this).setMessage("Sorry, database initialization error occurred.\n\n" +
+                    "For additional information, please refer to GitHub readme.")
+                    .setNegativeButton("QUIT", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, final int id) {
+                            finish();
+                        }}).setTitle("Error").setTitle("Fatal Error").setCancelable(false).show();
         }
         DatabaseOpenHelper dbHelper = new DatabaseOpenHelper(this);
         database = dbHelper.getReadableDatabase();
@@ -62,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * If new category, or data category was added, database structure was changed.
-     * Thus, code update is necessary to handle new data. Please refer to:
+     * Thus, code update is necessary to handle new data.
+     * Also, exceptions may happen even after you update the code during runtime. Please refer to:
      * https://github.com/paizhangpi/MacIndex/
      *
      * Not necessary if only new machine was added.
@@ -76,34 +83,46 @@ public class MainActivity extends AppCompatActivity {
         // Basic functionality was finished on 16:12 CST, Dec 2, 2019.
     }
 
-    private void initCategory(LinearLayout currentLayout, int category) {
-        Cursor cursor = database.query("category" + String.valueOf(category), null,
-                null, null, null, null, null);
-        while (cursor.moveToNext()) {
-            View mainChunk = getLayoutInflater().inflate(R.layout.chunk_main, null);
-            TextView machineName = mainChunk.findViewById(R.id.machineName);
-            Button viewButton = mainChunk.findViewById(R.id.viewButton);
-            // Create a String for each data category. Update here.
-            final String thisName = cursor.getString(cursor.getColumnIndex("name"));
-            final String thisProcessor = cursor.getString(cursor.getColumnIndex("processor"));
-            final String thisMaxRAM = cursor.getString(cursor.getColumnIndex("maxram"));
-            final String thisYear = cursor.getString(cursor.getColumnIndex("year"));
-            final String thisModel = cursor.getString(cursor.getColumnIndex("model"));
-            machineName.setText(thisName);
-            viewButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View unused) {
-                    Intent intent = new Intent(MainActivity.this, SpecsActivity.class);
-                    // Put each String to Specs Intent. Update here.
-                    intent.putExtra("name", thisName);
-                    intent.putExtra("processor", thisProcessor);
-                    intent.putExtra("maxram", thisMaxRAM);
-                    intent.putExtra("year", thisYear);
-                    intent.putExtra("model", thisModel);
-                    startActivity(intent);
-                }
-            });
-            currentLayout.addView(mainChunk);
+    private void initCategory(final LinearLayout currentLayout, final int category) {
+        try {
+            Cursor cursor = database.query("category" + String.valueOf(category), null,
+                    null, null, null, null, null);
+            while (cursor.moveToNext()) {
+                View mainChunk = getLayoutInflater().inflate(R.layout.chunk_main, null);
+                TextView machineName = mainChunk.findViewById(R.id.machineName);
+                Button viewButton = mainChunk.findViewById(R.id.viewButton);
+                // Create a String for each data category. Update here.
+                final String thisName = cursor.getString(cursor.getColumnIndex("name"));
+                final String thisProcessor = cursor.getString(cursor.getColumnIndex("processor"));
+                final String thisMaxRAM = cursor.getString(cursor.getColumnIndex("maxram"));
+                final String thisYear = cursor.getString(cursor.getColumnIndex("year"));
+                final String thisModel = cursor.getString(cursor.getColumnIndex("model"));
+                machineName.setText(thisName);
+                viewButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(final View unused) {
+                        Intent intent = new Intent(MainActivity.this, SpecsActivity.class);
+                        // Put each String to Specs Intent. Update here.
+                        intent.putExtra("name", thisName);
+                        intent.putExtra("processor", thisProcessor);
+                        intent.putExtra("maxram", thisMaxRAM);
+                        intent.putExtra("year", thisYear);
+                        intent.putExtra("model", thisModel);
+                        startActivity(intent);
+                    }
+                });
+                currentLayout.addView(mainChunk);
+            }
+        } catch (Exception e) {
+            new AlertDialog.Builder(this).setMessage("Sorry, database query error occurred.\n\n" +
+                    "For additional information, please refer to GitHub readme.")
+                    .setPositiveButton("DISMISS", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, final int id) {
+                            dialog.dismiss();
+                        }}).setNegativeButton("QUIT", new DialogInterface.OnClickListener() {
+                            public void onClick(final DialogInterface dialog, final int id) {
+                                finish();
+                            }}).setTitle("Error").setCancelable(false).show();
         }
     }
 }
