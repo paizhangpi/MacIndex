8bc36a097a84f3b74be4f7e22055baf60546a4ae
paizhangpi <paizhangpi@outlook.com>
2019-12-01 20:48:08 -0600

    Further update on database reading

--- app/src/main/java/com/macindex/macindex/MainActivity.java
+++ app/src/main/java/com/macindex/macindex/MainActivity.java
@@ -1,14 +1,55 @@
 package com.macindex.macindex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initDatabase();
+        DatabaseHelper db = new DatabaseHelper(this);
+        db.open();
+        db.close();
     }
+
+    private void initDatabase() {
+        String appDataPath = this.getApplicationInfo().dataDir;
+        File dbFolder = new File(appDataPath + "/databases");
+        dbFolder.mkdir();
+        File dbFilePath = new File(appDataPath + "/databases/specs.db");
+        try {
+            InputStream inputStream = this.getAssets().open("specs.db");
+            OutputStream outputStream = new FileOutputStream(dbFilePath);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer))>0)
+            {
+                outputStream.write(buffer, 0, length);
+            }
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+        } catch (IOException e){
+            // To catch
+        }
+    }
+
 }
